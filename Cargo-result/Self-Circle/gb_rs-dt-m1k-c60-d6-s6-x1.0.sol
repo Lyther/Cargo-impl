335d3b83d57d64ef77cbbd4f8d1fb8407aa9ced1
Enderaoe <enderaoelyther@gmail.com>
2020-01-06 08:54:11 +0800

    update more data

---
+++ Cargo-result/Self-Circle/gb_rs-dt-m1k-c60-d6-s6-x1.0.sol
@@ -0,0 +1,19 @@
+rs-dt-m1k-c60-d6-s6-x1.0
+dt
+Tot. number of vehicles     25
+Tot. number of customers    1000
+Base distance      (meters) 434169
+Solution distance  (meters) 159318
+  Total traveled   (meters) 99239
+  Penalty          (meters) 60079
+Solution matches            872
+Rejected                    0
+Avg. pickup delay     (sec) -1097
+Avg. trip delay       (sec) 906277473
+Avg. handle_customer   (ms) 0.465137
+Avg. handle_vehicle    (ms) 0
+Avg. match             (ms) 0
+Avg. listen            (ms) 5.72131
+Avg. number cust. per batch 9.28689
+Avg. number vehl. per batch 25
+Count shortest-path comps   634092
